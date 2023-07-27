fadfcb094a191d2d23c3b5312c7a67ad0aeba4d9
Simon Granier <simon.granier@sfr.fr>
2023-07-27 11:40:44 +0200

    DaoLivre et IDaoLivre

---
+++ projetFilRouge/src/main/java/com/inetum/pfr/projetFilRouge/dao/IDaoLivre.java
@@ -0,0 +1,8 @@
+package com.inetum.pfr.projetFilRouge.dao;
+
+import com.inetum.pfr.projetFilRouge.entity.Livre;
+
+// Hérite toutes ses méthodes de IDaoGeneric
+public interface IDaoLivre extends IDaoGeneric<Livre, Long>{
+	
+}
