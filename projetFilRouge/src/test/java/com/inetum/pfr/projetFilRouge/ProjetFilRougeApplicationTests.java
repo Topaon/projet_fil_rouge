d60428c7e4bc3a887bfa8e88f2d6b4703080bc39
fredericros <frederic.ros@gmail.com>
2023-07-27 09:27:45 +0200

    création Maven project

---
+++ projetFilRouge/src/test/java/com/inetum/pfr/projetFilRouge/ProjetFilRougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.pfr.projetFilRouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFilRougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
