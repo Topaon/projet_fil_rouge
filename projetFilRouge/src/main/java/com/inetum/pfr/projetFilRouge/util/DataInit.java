b20473d954d9225f1a8d2d0e74532001772985f7
Simon Granier <simon.granier@sfr.fr>
2023-07-27 16:49:35 +0200

    sef

--- projetFilRouge/src/main/java/com/inetum/pfr/projetFilRouge/util/DataInit.java
+++ projetFilRouge/src/main/java/com/inetum/pfr/projetFilRouge/util/DataInit.java
@@ -5,7 +5,9 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.inetum.pfr.projetFilRouge.dao.DaoDomaine;
 import com.inetum.pfr.projetFilRouge.dao.DaoLivre;
+import com.inetum.pfr.projetFilRouge.entity.Domaine;
 import com.inetum.pfr.projetFilRouge.entity.Livre;
 import com.inetum.pfr.projetFilRouge.entity.Livre.EtatLivre;
 
@@ -15,6 +17,9 @@ public class DataInit {
 	@Autowired
 	DaoLivre daoLivre;
 	
+	@Autowired
+	DaoDomaine daoDomaine;
+	
 	@PostConstruct
 	public void initializeDb() {
 		daoLivre.insert(new Livre (null, "titre1", "auteur1", "editeur1", true, EtatLivre.BON_ETAT));
@@ -22,5 +27,9 @@ public class DataInit {
 		daoLivre.insert(new Livre (null, "titre3", "auteur3", "editeur3", true, EtatLivre.BON_ETAT));
 		daoLivre.insert(new Livre (null, "titre4", "auteur4", "editeur4", true, EtatLivre.BON_ETAT));
 		daoLivre.insert(new Livre (null, "titre5", "auteur5", "editeur5", true, EtatLivre.BON_ETAT));
+		
+		daoDomaine.insert(new Domaine(null, "Aventure", "Ce livre parle d'aventure"));
+		daoDomaine.insert(new Domaine(null, "Amour", "Ce livre parle d'amour"));
+		daoDomaine.insert(new Domaine(null, "Policier", "Ce livre parle d'enquètes"));
 	}
 }
